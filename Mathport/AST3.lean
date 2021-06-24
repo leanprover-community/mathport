9c1dff1d885894c59de63135c70d1ca38f72c73e
Mario Carneiro <di.gama@gmail.com>
2021-06-23 22:48:29 -0400

    feat: Filling out the grammar

--- Mathport/AST3.lean
+++ Mathport/AST3.lean
@@ -5,41 +5,243 @@ Authors: Mario Carneiro, Daniel Selsam
 -/
 import Mathport.Util
 
+def Lean.BinderInfo.bracket : BinderInfo → Format → Format
+  | BinderInfo.default,        f => f.bracket "(" ")"
+  | BinderInfo.implicit,       f => f.bracket "{" "}"
+  | BinderInfo.strictImplicit, f => f.bracket "{{" "}}"
+  | BinderInfo.instImplicit,   f => f.bracket "[" "]"
+  | BinderInfo.auxDecl,        f => f.group
+
 namespace Mathport
 
-open Lean (Position)
+open Lean (Position Name BinderInfo)
+open Std (Format)
 
-structure Node3S (α : Type u) where
+structure Spanned (α : Type u) where
   start : Position
-  end_ : Option Position
+  end_ : Position
   kind  : α
   deriving Inhabited, Repr
 
-def Node3S.map (f : α → β) : Node3S α → Node3S β
+def Spanned.map (f : α → β) : Spanned α → Spanned β
   | ⟨s, e, a⟩ => ⟨s, e, f a⟩
+
+local prefix:max "*" => Spanned
+
+namespace AST3
+
+inductive VariableKind | «variable» | «parameter»
+
+instance : Repr VariableKind where
+  reprPrec
+  | VariableKind.«variable», _ => "variable"
+  | VariableKind.«parameter», _ => "parameter"
+
+inductive DeclKind
+| «axiom» | «constant» | «def» | «theorem»
+| «abbrev» | «example» | «instance»
+
+instance : Repr DeclKind where
+  reprPrec
+  | DeclKind.«axiom», _ => "axiom"
+  | DeclKind.«constant», _ => "constant"
+  | DeclKind.«def», _ => "def"
+  | DeclKind.«theorem», _ => "theorem"
+  | DeclKind.«abbrev», _ => "abbreviation"
+  | DeclKind.«example», _ => "example"
+  | DeclKind.«instance», _ => "instance"
+
+section
+set_option hygiene false
+local notation "Modifiers" => Array *Modifier
+local notation "Binders" => Array *Binder
+local notation "Node" => *NodeK
+local notation "Levels" => Option (Array *Level)
 
 mutual
 
-inductive Other3 : Type
-  | mk : (kind : String) → (children : Array (Node3S Node3K)) → Other3
-  deriving Inhabited, Repr
+inductive Other
+  | mk (kind : String) (value : Name) (children : Array (Option Node)) : Other
+  deriving Inhabited
 
-inductive Command3
-  | «prelude» : Command3
-  | other : Other3 → Command3
-  deriving Inhabited, Repr
+inductive Attribute
+  | mk (del : Bool) (name : *Name) (args : Array (Option Node)) : Attribute
+  deriving Inhabited
 
-inductive Node3K
-  | command : Command3 → Node3K
-  | other : Other3 → Node3K
-  deriving Inhabited, Repr
+inductive Modifier
+  | doc : String → Modifier
+  | attr («local» compact : Bool) : Array *Attribute → Modifier
+  | other : Other → Modifier
+  deriving Inhabited
+
+inductive Binder
+  | binder : BinderInfo → Option (Array *Name) → Binders → *Expr → Binder
+  | other : Other → Binder
+  deriving Inhabited
+
+inductive Level
+  | other : Other → Level
+  deriving Inhabited
+
+inductive Expr
+  | other : Other → Expr
+  deriving Inhabited
+
+inductive Arm
+  | mk (lhs : Array *Expr) (rhs : *Expr) : Arm
+  deriving Inhabited
+
+inductive DeclVal
+  | expr : Expr → DeclVal
+  | eqns : Array Arm → DeclVal
+  deriving Inhabited
+
+inductive Command
+  | «prelude» : Command
+  | «import» : Array *Name → Command
+  | mdoc : String → Command
+  | «universe» (var plural : Bool) : Array *Name → Command
+  | «namespace» : *Name → Command
+  | «section» : Option *Name → Command
+  | «end» : Option *Name → Command
+  | «variable» : VariableKind → Modifiers → (plural : Bool) → Binders → Command
+  | decl : DeclKind → Modifiers → Option *Name →
+    Levels → Binders → (ty : Option *Expr) → Option *DeclVal → Command
+  -- | mutual_decl : DeclKind → Modifiers → Binders → Command
+  | other : Other → Command
+  deriving Inhabited
+
+inductive NodeK
+  | name : Name → NodeK
+  | str : String → NodeK
+  | cmd : Command → NodeK
+  | mod : Modifier → NodeK
+  | binder : Binder → NodeK
+  | level : Level → NodeK
+  | expr : Expr → NodeK
+  | declVal : DeclVal → NodeK
+  | other : Other → NodeK
+  deriving Inhabited
+
+end
+end
+
+abbrev Modifiers := Array *Modifier
+abbrev Binders := Array *Binder
+abbrev Node := *NodeK
+abbrev Levels := Option (Array *Level)
+
+def spaced (f : α → Format) (mods : Array α) : Format :=
+  Format.join (mods.toList.map fun m => f m ++ Format.line)
+
+def suffix (pl : Bool) := if pl then "s " else " "
+mutual
+
+partial def optNode_repr : Option Node → Format
+  | none => ("⬝" : Format)
+  | some a => NodeK_repr a.kind
+
+partial def Other_repr : Other → Format
+  | ⟨k, v, c⟩ => k ++
+    (if v.isAnonymous then "" else "(" ++ v.toString ++ ")" ) ++
+    (if c.isEmpty then ("":Format) else
+      (("," ++ Format.line).joinSep (c.toList.map optNode_repr)).bracket "[" "]")
+
+partial def Attribute_repr : Attribute → Format
+  | ⟨del, n, args⟩ =>
+    (if del then "-" else "") ++ n.kind.toString ++
+    Format.join (args.toList.map fun
+      | none => ("⬝" : Format)
+      | some a => NodeK_repr a.kind)
+
+partial def Modifier_repr : Modifier → Format
+  | Modifier.doc s => ("/--" ++ s ++ "-/" : String)
+  | Modifier.attr l c attrs =>
+    (if l then "local " else "") ++
+    (Format.joinSep (attrs.toList.map fun u => Attribute_repr u.kind) ", ").bracket
+      (if c then "@[" else "attribute [") "]"
+  | Modifier.other n => Other_repr n
+
+partial def Binder_repr : Binder → Format
+  | Binder.binder bi none _ e => bi.bracket (Expr_repr e.kind)
+  | Binder.binder bi (some vars) bis e => bi.bracket $
+    spaced (fun v => v.kind.toString) vars ++ Binders_repr bis ++ ": " ++ Expr_repr e.kind
+  | Binder.other n => Other_repr n
+
+partial def Level_repr : Level → Format
+  | Level.other n => Other_repr n
+
+partial def Expr_repr : Expr → Format
+  | Expr.other n => Other_repr n
+
+partial def Arm_repr : Arm → Format
+  | ⟨lhs, rhs⟩ =>
+    "\n| " ++ Format.joinSep (lhs.toList.map fun e => Expr_repr e.kind) ", " ++
+    " := " ++ Expr_repr rhs.kind
+
+partial def DeclVal_repr : DeclVal → Format
+  | DeclVal.expr n => " :=\n" ++ Expr_repr n
+  | DeclVal.eqns arms => if arms.isEmpty then "." else
+    Format.join $ arms.toList.map Arm_repr
+
+partial def Modifiers_repr : Modifiers → Format := spaced fun m => Modifier_repr m.kind
+
+partial def Binders_repr : Binders → Format := spaced fun m => Binder_repr m.kind
+
+partial def optTy : Option *Expr → Format
+  | none => ""
+  | some e => ": " ++ Expr_repr e.kind
+
+partial def Levels_repr : Levels → Format
+  | none => ""
+  | some us => (Format.joinSep (us.toList.map fun u => Level_repr u.kind) ", ").bracket ".{" "}"
+
+partial def Command_repr : Command → Format
+  | Command.«prelude» => "prelude"
+  | Command.«import» ns => "import " ++ Format.joinSep (ns.toList.map fun a => a.kind.toString) " "
+  | Command.mdoc s => ("/-!" ++ s ++ "-/" : String)
+  | Command.«universe» var pl ns =>
+    "universe" ++ (if var then " variable" else "") ++ suffix pl ++
+    Format.join (ns.toList.map fun a => (" ":Format) ++ a.kind.toString)
+  | Command.«namespace» n => ("namespace ":Format) ++ n.kind.toString
+  | Command.«section» (some n) => ("section ":Format) ++ n.kind.toString
+  | Command.«section» none => "section"
+  | Command.«end» (some n) => ("end ":Format) ++ n.kind.toString
+  | Command.«end» none => "end"
+  | Command.«variable» vk mods plural bis =>
+    Modifiers_repr mods ++ repr vk ++ (if plural then "s " else " ") ++ Binders_repr bis
+  | Command.decl dk mods n us bis ty val =>
+    Modifiers_repr mods ++ repr dk ++
+    (match n with | none => "" | some n => " " ++ n.kind.toString : String) ++
+    Levels_repr us ++ " " ++ Binders_repr bis ++
+    (match ty with | none => "" | some ty => ": " ++ Expr_repr ty.kind) ++
+    (match val with | none => "" | some val => DeclVal_repr val.kind)
+  -- | Command.mutual_decl dk mods bis =>
+  --   Modifiers_repr mods ++ repr dk ++ " " ++ Binders_repr bis
+  | Command.other n => Other_repr n
+
+partial def NodeK_repr : NodeK → Format
+  | NodeK.name n => n.toString
+  | NodeK.str n => repr n
+  | NodeK.cmd n => Command_repr n
+  | NodeK.mod n => Modifier_repr n
+  | NodeK.binder n => Binder_repr n
+  | NodeK.level n => Level_repr n
+  | NodeK.expr n => Expr_repr n
+  | NodeK.declVal n => DeclVal_repr n
+  | NodeK.other n => Other_repr n
 
 end
 
-abbrev Node3 := Node3S Node3K
+instance : Repr Other := ⟨fun n _ => Other_repr n⟩
+instance : Repr Command := ⟨fun n _ => Command_repr n⟩
+instance : Repr NodeK := ⟨fun n _ => NodeK_repr n⟩
+
+
+end AST3
 
 structure AST3 where
-  commands : Array (Node3S Command3)
+  commands : Array (Spanned AST3.Command)
   deriving Inhabited, Repr
 
 end Mathport
--- Mathport/Parse.lean
+++ Mathport/Parse.lean
@@ -3,12 +3,13 @@ Copyright (c) 2021 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Daniel Selsam
 -/
+import Lean.Expr
 import Mathport.Util
 import Mathport.AST3
 
 namespace Mathport
 
-open Lean (Json FromJson Position Name)
+open Lean (Json FromJson Position Name BinderInfo)
 open Lean.FromJson (fromJson?)
 
 namespace Parse
@@ -24,6 +25,9 @@ structure RawNode3 where
   children : Option (Array AstId)
   deriving FromJson, Repr
 
+def RawNode3.children' (n : RawNode3) : Array AstId := n.children.getD #[]
+def RawNode3.end' (n : RawNode3) : Position := n.end.getD n.start
+
 structure RawAST3 where
   ast      : Array (Option RawNode3)
   commands : Array AstId
@@ -31,54 +35,170 @@ structure RawAST3 where
   -- expr     : Array RawExpr
   deriving FromJson
 
-abbrev M := ReaderT (Array (Option RawNode3)) $ StateT (HashMap AstId Node3) $ Except String
+open AST3
+
+abbrev M := ReaderT (Array (Option RawNode3)) $ StateT (HashMap AstId Node) $ Except String
+
+def RawNode3.map (n : RawNode3) (f : String → Name → Array AstId → M α) : M (Spanned α) := do
+  pure ⟨n.start, n.end', ← f n.kind n.value n.children'⟩
+
+def opt (f : AstId → M α) (i : AstId) : M (Option α) :=
+  if i = 0 then pure none else some <$> f i
 
 def getRaw (i : AstId) : M RawNode3 := do
   match (← read)[i] with
   | some a => a
   | none => throw "missing node"
 
-def getRaw? (i : AstId) : M (Option RawNode3) := do
-  if i = 0 then pure none else some <$> getRaw i
+def getRaw? : AstId → M (Option RawNode3) := opt getRaw
 
-def withRaw'
-  (mk : Node3 → α → M β)
-  (f : α → Node3K) (g : Node3 → Option α)
-  (m : String → Array AstId → M α)
-  (i : AstId) : M β := do
+def withRaw' (mk : Node → α → M β) (f : α → NodeK) (g : Node → Option α)
+  (m : String → Name → Array AstId → M α) (i : AstId) : M β := do
   match (← get).find? i with
   | some n => match g n with
     | some k => mk n k
     | none => throw "type error"
   | none =>
     let r ← getRaw i
-    let a ← m r.kind (r.children.getD #[])
-    let n := { start := r.start, end_ := r.«end», kind := f a }
+    let a ← m r.kind r.value r.children'
+    let n := { start := r.start, end_ := r.end', kind := f a }
     modify fun s => s.insert i n
     mk n a
 
-def withRawK (f : α → Node3K) (g : Node3K → Option α) (m : String → Array AstId → M α) (i : AstId) : M α := do
+def withRawK (f : α → NodeK) (g : NodeK → Option α) (m : String → Name → Array AstId → M α) (i : AstId) : M α := do
   withRaw' (fun _ => pure) f (fun n => g n.kind) m i
 
-def withRaw (f : α → Node3K) (g : Node3K → Option α) (m : String → Array AstId → M α) (i : AstId) : M (Node3S α) := do
+def withRaw (f : α → NodeK) (g : NodeK → Option α) (m : String → Name → Array AstId → M α) (i : AstId) : M (Spanned α) := do
   withRaw' (fun n a => n.map fun _ => a) f (fun n => g n.kind) m i
 
 mutual
 
-partial def getNode (i : AstId) : M Node3 := do
-  withRaw' (fun n _ => n) id (fun n => some n.kind) (fun k c => Node3K.other <$> mkOther k c) i
+partial def getNode (i : AstId) : M Node := do
+  withRaw' (fun n _ => n) id (fun n => some n.kind) (fun k v c => NodeK.other <$> mkOther k v c) i
 
-partial def mkOther (k : String) (c : Array AstId) : M Other3 := Other3.mk k <$> c.mapM getNode
+partial def getNode? : AstId → M (Option Node) := opt getNode
+
+partial def mkOther (k : String) (v : Name) (c : Array AstId) : M Other := Other.mk k v <$> c.mapM getNode?
 
 end
 
-open Command3 in
-def getCommand : AstId → M (Node3S Command3) :=
-  withRaw Node3K.command (fun | Node3K.command c => some c | _ => none) aux
+def getName : AstId → M (Spanned Name) :=
+  withRaw NodeK.name (fun | NodeK.name c => some c | _ => none) (fun _ v _ => v)
+
+def getStr : AstId → M (Spanned String) :=
+  withRaw NodeK.str (fun | NodeK.str c => some c | _ => none) (fun _ v _ => v.getString!)
+
+def arr (f : AstId → M α) (i : AstId) : M (Array α) := do
+  match ← getRaw? i with
+  | some n => n.children'.mapM f
+  | _ => pure #[]
+
+open Level in
+def getLevel : AstId → M (Spanned Level) :=
+  withRaw NodeK.level (fun | NodeK.level c => some c | _ => none) aux
 where
-  aux : String → Array AstId → M Command3
-  | "prelude", _ => «prelude»
-  | k, args => other <$> mkOther k args
+  aux : String → Name → Array AstId → M Level
+  | k, v, args => other <$> mkOther k v args
+
+def getLevels : AstId → M (Option (Array (Spanned Level))) := opt (arr getLevel)
+
+open Expr in
+def getExpr : AstId → M (Spanned Expr) :=
+  withRaw NodeK.expr (fun | NodeK.expr c => some c | _ => none) aux
+where
+  aux : String → Name → Array AstId → M Expr
+  | k, v, args => other <$> mkOther k v args
+
+mutual
+
+partial def getBinder : AstId → M (Spanned Binder) :=
+  withRaw NodeK.binder (fun | NodeK.binder c => some c | _ => none) aux
+where
+  aux : String → Name → Array AstId → M Binder
+  | "binder_0", _, args => binder BinderInfo.default args
+  | "binder_1", _, args => binder BinderInfo.instImplicit args
+  | "binder_2", _, args => binder BinderInfo.strictImplicit args
+  | "binder_4", _, args => binder BinderInfo.implicit args
+  | "binder_8", _, args => binder BinderInfo.auxDecl args
+  | k, v, args => Binder.other <$> mkOther k v args
+
+  binder (bi : BinderInfo) (args : Array AstId) : M Binder := do
+    Binder.binder bi (← opt (arr getName) args[0]) (← getBinders args[1]) (← getExpr args[2])
+
+partial def getBinders : AstId → M Binders := arr getBinder
+
+end
+
+def getAttr (i : AstId) : M (Spanned Attribute) := do
+  (← getRaw i).map fun _ _ args => do
+    pure ⟨args[0] ≠ 0, ← getName args[1], ← args[2:].toArray.mapM getNode?⟩
+
+open DeclVal in
+def getDeclVal : AstId → M (Spanned DeclVal) :=
+  withRaw NodeK.declVal (fun | NodeK.declVal c => some c | _ => none) aux
+where
+  aux : String → Name → Array AstId → M DeclVal
+  | "eqns", _, args => eqns <$> args.mapM getArm
+  | k, v, args => expr <$> getExpr.aux k v args
+
+  getArm (i : AstId) : M Arm := do
+    let args := (← getRaw i).children'
+    pure ⟨← arr getExpr args[0], ← getExpr args[1]⟩
+
+open Modifier in
+def getModifier : AstId → M (Spanned Modifier) :=
+  withRaw NodeK.mod (fun | NodeK.mod c => some c | _ => none) aux
+where
+  aux : String → Name → Array AstId → M Modifier
+  | "doc", v, _ => doc v.getString!
+  | "@[", _, args => attr false true <$> arr getAttr args[0]
+  | k, v, args => other <$> mkOther k v args
+
+def getModifiers : AstId → M Modifiers := arr getModifier
+
+-- def getVariable (vk : VariableKind) (args : Array AstId) : M Command := do
+--   Command.«variable» vk (← getModifiers args[0]) false #[← getBinder args[1]]
+
+-- def getVariables (vk : VariableKind) (args : Array AstId) : M Command := do
+--   Command.«variable» vk (← getModifiers args[0]) true <$> args[1:].toArray.mapM getBinder
+
+open Command in
+def getCommand : AstId → M (Spanned Command) :=
+  withRaw NodeK.cmd (fun | NodeK.cmd c => some c | _ => none) aux
+where
+  aux : String → Name → Array AstId → M Command
+  | "prelude", _, _ => «prelude»
+  | "import", _, args => «import» <$> args.mapM getName
+  | "mdoc", v, _ => mdoc v.getString!
+  | "namespace", _, args => «namespace» <$> getName args[0]
+  | "section", _, args => «section» <$> opt getName args[0]
+  | "end", _, args => «end» <$> opt getName args[0]
+  | "universe", _, args => «universe» false false <$> args.mapM getName
+  | "universes", _, args => «universe» false true <$> args.mapM getName
+  | "universe_variable", _, args => «universe» true false <$> args.mapM getName
+  | "universe_variables", _, args => «universe» true true <$> args.mapM getName
+  | "variable", _, args => getVariable VariableKind.variable false args
+  | "parameter", _, args => getVariable VariableKind.parameter false args
+  | "variables", _, args => getVariable VariableKind.variable true args
+  | "parameters", _, args => getVariable VariableKind.parameter true args
+  | "definition", _, args => getDecl DeclKind.def args
+  | k, v, args => other <$> mkOther k v args
+
+  getVariable (vk pl) (args : Array AstId) : M Command := do
+    Command.«variable» vk (← getModifiers args[0]) pl <$> args[1:].toArray.mapM getBinder
+
+  getHeader (args : Subarray AstId) : M _ := do
+    (← getLevels args[0], ← opt getName args[1], ← getBinders args[2], ← opt getExpr args[3])
+
+  getDecl (dk) (args : Array AstId) : M Command := do
+    let mods ← getModifiers args[0]
+    if args[1] = 0 then
+      let (us, n, bis, ty) ← getHeader args[2:6]
+      let val ← opt getDeclVal args[6]
+      Command.decl dk mods n us bis ty val
+    else
+      -- Command.mutual_decl dk mods <$> args[1:].toArray.mapM getBinder
+      panic! "mutual"
 
 def RawAST3.toAST3 : RawAST3 → Except String AST3
 | ⟨ast, commands⟩ => commands.mapM getCommand |>.run ast |>.run' {} |>.map AST3.mk
@@ -94,3 +214,11 @@ def parseAST3 (filename : System.FilePath) : IO AST3 := do
   let rawAST3 ← fromJson? json (α := Parse.RawAST3)
   println! "Converting RawAST3 to AST3..."
   rawAST3.toAST3
+
+-- #eval show IO Unit from do
+--   let s ← IO.FS.readFile "/home/mario/Documents/lean/lean/library/init/function.ast.json"
+--   let json ← Json.parse s
+--   let rawAST3 ← fromJson? json (α := Parse.RawAST3)
+--   let ⟨ast⟩ ← rawAST3.toAST3
+--   for c in ast[0:10] do
+--     println! (repr c.kind).group ++ "\n"
