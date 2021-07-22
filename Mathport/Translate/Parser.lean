9a7a15b0f73b5fdda8fb137b32c85b41e9cc3dfa
Mario Carneiro <di.gama@gmail.com>
2021-07-21 20:01:05 -0400

    feat: work on builtins and parsing infrastructure

--- Mathport/Translate/Parser.lean
+++ Mathport/Translate/Parser.lean
@@ -8,7 +8,8 @@ import Mathport.Data4
 import Mathport.AST3
 
 open Std (HashMap)
-open Lean Elab Tactic
+open Lean hiding Expr
+open Lean.Elab Tactic
 
 namespace Mathport
 namespace Translate
@@ -19,18 +20,18 @@ namespace Parser
 
 structure Context where
   cmds : Array Command
-  arr : Array VMCall
+  arr : Array (Spanned VMCall)
 
 abbrev ParserM := ReaderT Context $ StateT Nat OptionM
 
 def next : ParserM VMCall := fun s i =>
-  if h : i < s.arr.size then pure (s.arr.get ⟨i, h⟩, i+1) else failure
+  if h : i < s.arr.size then pure ((s.arr.get ⟨i, h⟩).kind, i+1) else failure
 
 def ident : ParserM Name := do let VMCall.ident n ← next | failure; n
 
 def smallNat : ParserM Nat := do let VMCall.nat n ← next | failure; n
 
-def pExpr : (pat :_:= false) → ParserM AST3.Expr
+def pExpr : (pat :_:= false) → ParserM Expr
   | false => do let VMCall.expr e ← next | failure; e
   | true => do let VMCall.pat e ← next | failure; e
 
@@ -43,23 +44,28 @@ def tk (tk : String) : ParserM Unit := do
   let VMCall.token t ← next | failure
   guard (tk = t)
 
-partial def many (x : ParserM α) : ParserM (List α) :=
-  (do (← x) :: (← many x)) <|> pure []
+partial def manyList (x : ParserM α) : ParserM (List α) :=
+  (do (← x) :: (← manyList x)) <|> pure []
 
-local postfix "?" => optional
-local postfix "*" => many
+def many (x : ParserM α) : ParserM (Array α) := List.toArray <$> manyList x
 
-def sepBy (s : ParserM Unit) (p : ParserM α) : ParserM (List α) :=
-  (do (← p) :: (← (s *> p)*)) <|> pure []
+scoped postfix:max "?" => optional
+scoped postfix:max "*" => many
+
+def sepByList (s : ParserM Unit) (p : ParserM α) : ParserM (List α) :=
+  (do (← p) :: (← manyList (s *> p))) <|> pure []
+
+def sepBy (s : ParserM Unit) (p : ParserM α) : ParserM (Array α) := List.toArray <$> sepByList s p
 
 def brackets (l r) (p : ParserM α) := tk l *> p <* tk r
 def listOf (p : ParserM α) := brackets "[" "]" $ sepBy (tk ",") p
+def maybeListOf (p : ParserM α) := listOf p <|> do #[← p]
 def ident_ := ident <|> tk "_" *> `_
 def usingIdent := (tk "using" *> ident)?
-def withIdentList := (tk "with" *> ident_*) <|> pure []
-def withoutIdentList := (tk "without" *> ident*) <|> pure []
+def withIdentList := (tk "with" *> ident_*) <|> pure #[]
+def withoutIdentList := (tk "without" *> ident*) <|> pure #[]
 
-def Lean.Elab.Tactic.Location.ofOption (l : List (Option Name)) : Location :=
+def Lean.Elab.Tactic.Location.ofOption (l : Array (Option Name)) : Location :=
   let (hs, ty) := l.foldl (init := (#[], false)) fun
     | (hs, ty), none => (hs, true)
     | (hs, ty), some n => (hs.push n, ty)
@@ -71,8 +77,8 @@ def location := (tk "at" *> (tk "*" *> pure Location.wildcard <|>
   pure (Location.targets #[] true)
 
 def pExprList := listOf pExpr
-def optPExprList := listOf pExpr <|> pure []
-def pExprListOrTExpr := listOf pExpr <|> (do [← pExpr])
+def optPExprList := listOf pExpr <|> pure #[]
+def pExprListOrTExpr := maybeListOf pExpr
 def onlyFlag := (tk "only" *> true) <|> pure false
 
 def parseBinders : ParserM Binders := do let VMCall.binders bis ← next | failure; bis
@@ -81,5 +87,51 @@ def inductiveDecl : ParserM InductiveCmd := do
   let VMCall.inductive i ← next | failure
   let Command.inductive c ← (← read).cmds[i] | failure
   c
+
+def renameArg : ParserM (Name × Name) := do (← ident, ← (tk "->")? *> ident)
+
+def renameArgs : ParserM (Array (Name × Name)) :=
+  (do #[← renameArg]) <|> listOf renameArg
+
+structure RwRule where
+  symm : Bool
+  rule : Expr
+
+def rwRule : ParserM RwRule := do
+  pure ⟨Option.isSome (← (tk "←" <|> tk "<-")?), ← pExpr⟩
+
+def rwRules : ParserM (Array RwRule) := maybeListOf rwRule
+
+def generalizeArg : ParserM (Expr × Name) := do
+  let AST3.Expr.app e rhs ← (← pExpr).unparen | failure
+  let AST3.Expr.app _ lhs ← e.kind.unparen | failure
+  let AST3.Expr.ident x ← rhs.kind.unparen | failure
+  (lhs.kind, x)
+
+def casesArg : ParserM (Option Name × Expr) := do
+  let t ← pExpr
+  match t.unparen with
+  | AST3.Expr.ident x => (some x, ← tk ":" *> pExpr)
+  | _ => (none, t)
+
+def caseArg : ParserM (Array Name × Option (Array Name)) := do
+  (← ident_*, ← (tk ":" *> ident_*)?)
+
+def case : ParserM (Array (Array Name × Option (Array Name))) := maybeListOf caseArg
+
+inductive SimpArg
+| allHyps
+| except (n : Name)
+| expr (e : Expr)
+| symmExpr (e : Expr)
+
+def simpArg : ParserM SimpArg :=
+  (tk "*" *> SimpArg.allHyps) <|>
+  (tk "-" *> do SimpArg.except (← ident)) <|>
+  (tk "<-" *> do SimpArg.symmExpr (← pExpr)) <|>
+  do SimpArg.expr (← pExpr)
+
+def simpArgList : ParserM (Array SimpArg) :=
+  (tk "*" *> #[SimpArg.allHyps]) <|> listOf simpArg <|> pure #[]
 
 end Parser
--- Mathport/Translate/Tactic.lean
+++ Mathport/Translate/Tactic.lean
@@ -3,23 +3,192 @@ Copyright (c) 2021 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
-import Std.Data.HashMap
 import Mathport.Translate.Basic
+import Mathport.Translate.Parser
 
-open Std (HashMap)
-open Lean Elab Tactic
+open Lean (Syntax Name mkIdent mkNode mkAtom)
 
 namespace Mathport
 namespace Translate
 
-open AST3
+open AST3 Parser
 
 namespace Tactic
 
-def trRw : Array Param → M Syntax
-  | #[Param.parse _ q, Param.parse _ loc] => throw! "rw {repr (q, loc)}"
-  | #[Param.parse _ q, Param.parse _ loc, Param.expr cfg] => throw! "unsupported (impossible)"
-  | args => throw! "unsupported (impossible)"
+structure Context where
+  args : Array (Spanned Param)
 
-def builtinTactics : List (Name × (Array Param → M Syntax)) :=
-  [(`rw, trRw)]
+structure State where
+  pos : Nat
+
+abbrev TacM := ReaderT Context $ StateT State M
+
+def next? : TacM (Option Param) := do
+  let args := (← read).args
+  let i := (← get).pos
+  if h : i < args.size then
+    modify fun s => { s with pos := i+1 }
+    (args.get ⟨i, h⟩).kind
+  else none
+
+def next! : TacM Param := do
+  match ← next? with | some p => p | none => throw! "missing argument"
+
+def parse (p : Parser.ParserM α) : TacM α := do
+  let Param.parse _ args ← next! | throw! "expecting parse arg"
+  match p ⟨(← readThe Translate.Context).commands, args⟩ |>.run' 0 with
+  | none => throw! "parse error"
+  | some a => a
+
+def expr? : TacM (Option AST3.Expr) := do
+  match ← next? with
+  | none => none
+  | some (Param.expr e) => some e.kind
+  | _ => throw! "parse error"
+
+def expr! : TacM AST3.Expr := do
+  match ← expr? with | some p => p | none => throw! "missing argument"
+
+def itactic : TacM AST3.Block := do
+  let Param.block bl ← next! | throw! "expecting tactic arg"
+  bl
+
+-- TODO(Mario): Why aren't the scoped notations working?
+local postfix:max "?" => optional
+local postfix:max "*" => many
+
+syntax "propagateTags " tactic : tactic
+
+def trPropagateTags : TacM Syntax := do
+  `(tactic| propagateTags $(← trBlock (← itactic)))
+
+def trIntro : TacM Syntax := do
+  match ← parse (ident_)? with
+  | none => `(tactic| intro)
+  | some h => `(tactic| intro $(mkIdent h))
+
+def trIntros : TacM Syntax := do
+  match ← parse (ident_)* with
+  | #[] => `(tactic| intros)
+  | hs => `(tactic| intro $[$(hs.map mkIdent)]*)
+
+def trIntrov : TacM Syntax := do
+  let hs ← parse (ident_)*
+  throw! "unsupported"
+
+syntax renameArg := ident " => " ident
+syntax "rename' " renameArg,* : tactic
+def trRenameArg : Name × Name → M Syntax
+  | (x, y) => mkNode ``renameArg #[mkIdent x, mkAtom "=>", mkIdent y]
+
+def trRename : TacM Syntax := do
+  let renames ← parse renameArgs
+  `(tactic| rename' $[$(← (renames.mapM trRenameArg : M _))],*)
+
+def trRw : TacM Syntax := do
+  let q ← parse rwRules
+  let loc ← parse location
+  let cfg ← expr?
+  throw! "unsupported"
+
+def builtinTactics : List (Name × TacM Syntax) := [
+  (`propagate_tags, trPropagateTags),
+  (`intro, trIntro),
+  (`intros, trIntros),
+  (`introv, trIntrov),
+  (`rename, trRename),
+  (`apply, throw "unsupported"),
+  (`fapply, throw "unsupported"),
+  (`eapply, throw "unsupported"),
+  (`apply_with, throw "unsupported"),
+  (`mapply, throw "unsupported"),
+  (`apply_instance, throw "unsupported"),
+  (`refine, throw "unsupported"),
+  (`assumption, throw "unsupported"),
+  (`assumption', throw "unsupported"),
+  (`change, throw "unsupported"),
+  (`exact, throw "unsupported"),
+  (`from, throw "unsupported"),
+  (`revert, throw "unsupported"),
+  (`to_expr', throw "unsupported"),
+  (`rewrite, trRw),
+  (`rw, trRw),
+  (`rwa, throw "unsupported"),
+  (`erewrite, throw "unsupported"),
+  (`erw, throw "unsupported"),
+  (`with_cases, throw "unsupported"),
+  (`generalize, throw "unsupported"),
+  (`induction, throw "unsupported"),
+  (`case, throw "unsupported"),
+  (`destruct, throw "unsupported"),
+  (`cases, throw "unsupported"),
+  (`cases_matching, throw "unsupported"),
+  (`casesm, throw "unsupported"),
+  (`cases_type, throw "unsupported"),
+  (`trivial, throw "unsupported"),
+  (`admit, throw "unsupported"),
+  (`sorry, throw "unsupported"),
+  (`contradiction, throw "unsupported"),
+  (`iterate, throw "unsupported"),
+  (`repeat, throw "unsupported"),
+  (`try, throw "unsupported"),
+  (`skip, throw "unsupported"),
+  (`solve1, throw "unsupported"),
+  (`abstract, throw "unsupported"),
+  (`all_goals, throw "unsupported"),
+  (`any_goals, throw "unsupported"),
+  (`focus, throw "unsupported"),
+  (`assume_core, throw "unsupported"),
+  (`assume, throw "unsupported"),
+  (`have, throw "unsupported"),
+  (`let, throw "unsupported"),
+  (`suffices, throw "unsupported"),
+  (`trace_state, throw "unsupported"),
+  (`trace, throw "unsupported"),
+  (`existsi, throw "unsupported"),
+  (`constructor, throw "unsupported"),
+  (`econstructor, throw "unsupported"),
+  (`left, throw "unsupported"),
+  (`right, throw "unsupported"),
+  (`split, throw "unsupported"),
+  (`contructor_matching, throw "unsupported"),
+  (`exfalso, throw "unsupported"),
+  (`injection, throw "unsupported"),
+  (`injections, throw "unsupported"),
+  (`simp, throw "unsupported"),
+  (`trace_simp_set, throw "unsupported"),
+  (`simp_intros, throw "unsupported"),
+  (`dsimp, throw "unsupported"),
+  (`reflexivity, throw "unsupported"),
+  (`refl, throw "unsupported"),
+  (`symmetry, throw "unsupported"),
+  (`transitivity, throw "unsupported"),
+  (`ac_reflexivity, throw "unsupported"),
+  (`ac_refl, throw "unsupported"),
+  (`cc, throw "unsupported"),
+  (`subst, throw "unsupported"),
+  (`subst_vars, throw "unsupported"),
+  (`clear, throw "unsupported"),
+  (`dunfold, throw "unsupported"),
+  (`delta, throw "unsupported"),
+  (`unfold_projs, throw "unsupported"),
+  (`unfold, throw "unsupported"),
+  (`unfold1, throw "unsupported"),
+  (`apply_opt_param, throw "unsupported"),
+  (`apply_auto_param, throw "unsupported"),
+  (`fail_if_success, throw "unsupported"),
+  (`success_if_fail, throw "unsupported"),
+  (`guard_expr_eq, throw "unsupported"),
+  (`guard_target, throw "unsupported"),
+  (`guard_hyp, throw "unsupported"),
+  (`match_target, throw "unsupported"),
+  (`by_cases, throw "unsupported"),
+  (`funext, throw "unsupported"),
+  (`by_contradiction, throw "unsupported"),
+  (`by_contra, throw "unsupported"),
+  (`type_check, throw "unsupported"),
+  (`done, throw "unsupported"),
+  (`show, throw "unsupported"),
+  (`specialize, throw "unsupported"),
+  (`congr, throw "unsupported")
+]
