4a6ee8ddf4fada8db7b3193baf8e2b57cfde9c53
Daniel Selsam <daniel.selsam@protonmail.com>
2021-08-17 16:44:26 -0700

    chore: rm stale PostPort/Coe.lean

--- Lib4/Mathlib/PostPort.lean
+++ Lib4/Mathlib/PostPort.lean
@@ -3,5 +3,4 @@ Copyright (c) 2021 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Daniel Selsam
 -/
-import Mathlib.PostPort.Coe
 import Mathlib.PostPort.Obviously
