33b28efdb73a57f308aec9335d4051299fd400f2
Daniel Selsam <daniel.selsam@protonmail.com>
2021-10-05 21:04:48 -0700

    chore: rm redundant mathlib4 dependency

--- Lib4/leanbin/lakefile.lean
+++ Lib4/leanbin/lakefile.lean
@@ -7,11 +7,6 @@ package leanbin {
   libGlobs := #[],
   dependencies := #[
   {
-    name := "mathlib",
-    src := Source.git "https://github.com/dselsam/mathlib4.git" "lake" none,
-    dir := FilePath.mk "."
-  },
-  {
     name := "mathport",
     src := Source.git "https://github.com/leanprover/mathport.git" "master" none,
     dir := FilePath.mk "."
