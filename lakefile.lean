4e601d97dc49ff61cf224ef5ede3285c3f9cd5da
Scott Morrison <scott.morrison@gmail.com>
2021-10-19 19:58:06 +1100

    gah, branch and rev in wrong order

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package mathport {
     -- as changes to tactics in mathlib4 often cause breakages here,
     -- particularly in `Mathport/Syntax/Translate/Tactic/Mathlib.lean`.
     -- We'll need to keep updating that file, and bumping the commit here.
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "master" "b28a3d51e722d8b43367035e0eb5790b4cb6da53",
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "b28a3d51e722d8b43367035e0eb5790b4cb6da53",
     dir := FilePath.mk "."
   }],
   binRoot := `MathportApp
