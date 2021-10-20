2571fe28be22e69f722b037757670118a0fe57bd
Scott Morrison <scott.morrison@gmail.com>
2021-10-20 17:00:45 +1100

    experiment: remove the dependency of leanbin on mathport; see what happens

--- Lib4/leanbin/lakefile.lean
+++ Lib4/leanbin/lakefile.lean
@@ -4,12 +4,12 @@ open Lake DSL System
 
 package leanbin {
   libRoots := #[],
-  libGlobs := #[],
-  dependencies := #[
-  {
-    name := "mathport",
-    src := Source.git "https://github.com/leanprover/mathport.git" "master" none,
-    dir := FilePath.mk "."
-  }
-  ]
+  libGlobs := #[]
+  -- dependencies := #[
+  -- {
+  --   name := "mathport",
+  --   src := Source.git "https://github.com/leanprover/mathport.git" "master" none,
+  --   dir := FilePath.mk "."
+  -- }
+  -- ]
 }
