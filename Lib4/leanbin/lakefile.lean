39a49f411b1389c25610c50d286997ca7713fab1
Scott Morrison <scott.morrison@gmail.com>
2021-10-21 17:59:31 +1100

    .

--- Lib4/leanbin/lakefile.lean
+++ Lib4/leanbin/lakefile.lean
@@ -5,11 +5,4 @@ open Lake DSL System
 package leanbin {
   libRoots := #[],
   libGlobs := #[]
-  -- dependencies := #[
-  -- {
-  --   name := "mathport",
-  --   src := Source.git "https://github.com/leanprover/mathport.git" "master" none,
-  --   dir := FilePath.mk "."
-  -- }
-  -- ]
 }
