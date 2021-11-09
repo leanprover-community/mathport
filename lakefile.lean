55bc920470fb8064d04389b2d385d9bd4f1f8544
Scott Morrison <scott.morrison@gmail.com>
2021-11-09 20:31:18 +1100

    bumping mathlib4, so we should be at latest Lean 4 too

--- lakefile.lean
+++ lakefile.lean
@@ -6,10 +6,8 @@ package mathport {
   dependencies := #[{
     name := "mathlib",
     -- We point to a particular commit in mathlib4,
-    -- as changes to tactics in mathlib4 often cause breakages here,
-    -- particularly in `Mathport/Syntax/Translate/Tactic/Mathlib.lean`.
-    -- We'll need to keep updating that file, and bumping the commit here.
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "b05d1f205209aa08422d0aecc8a8768787f49d91",
+    -- as changes to tactics in mathlib4 may cause breakages here,
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "d4ec6b4717e21193fee95da3b94c089123a4bc3b",
     dir := FilePath.mk "."
   }],
   binRoot := `MathportApp
