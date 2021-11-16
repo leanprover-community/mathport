34a69743f3a52fd8244cf5106aac378225a592a9
Scott Morrison <scott.morrison@gmail.com>
2021-11-16 17:57:57 +1100

    see if it works on Hoskinson

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,9 @@ package mathport {
   dependencies := #[{
     name := "mathlib",
     -- We point to a particular commit in mathlib4,
-    -- as changes to tactics in mathlib4 may cause breakages here,
+    -- as changes to tactics in mathlib4 may cause breakages here.
+    -- Please ensure that `lean-toolchain` points to the same release of Lean 4
+    -- as this commit of mathlib4 uses.
     src := Source.git "https://github.com/leanprover-community/mathlib4.git" "64f9c43eb9a75fb4c5989ac711623d06e9696e60",
   }],
   binRoot := `MathportApp
