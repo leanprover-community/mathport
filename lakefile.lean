a1da58f22829b7ce359302418cbbe717ad5bf56c
Scott Morrison <scott.morrison@gmail.com>
2021-11-23 11:02:16 +1100

    fix: mathlib4 commit sha

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package mathport {
     -- as changes to tactics in mathlib4 may cause breakages here.
     -- Please ensure that `lean-toolchain` points to the same release of Lean 4
     -- as this commit of mathlib4 uses.
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "75b0024c6e720126877e415c744bbe1f01e841e5"
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "bc7ef82aae2ab89845a4e496bb9f1aac08679828"
   }],
   binRoot := `MathportApp
   moreLinkArgs := if Platform.isWindows then #[] else #["-rdynamic"]
