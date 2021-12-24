b49e0c6c8a359df015e1b4ea9121d6e50d78dd3e
Gabriel Ebner <gebner@gebner.org>
2021-12-24 11:05:21 +0100

    chore: add more transform rules (#87)
    * Add more transform rules.
    
    * chore: also rewrite let/suffices
    
    * Disable `add := fun x y => ...` transformation for now.
    
    * Transform `by · $seq`
    
    * Expand `by (skip; skip)` to `by skip; skip`

--- Mathport/Syntax/Transform/Declaration.lean
+++ Mathport/Syntax/Transform/Declaration.lean
@@ -10,8 +10,13 @@ mathport_rules
       { $[$fieldName:ident := $fieldVal:term $[,]?]* }) =>
     `($mods:declModifiers $attr:attrKind instance $[$prio:namedPrio]? $[$id:declId]? $sig:declSig where
         $[$fieldName:ident := $fieldVal:term]*)
+  | `($mods:declModifiers def $id:declId $sig:optDeclSig :=
+        { $[$fieldName:ident := $fieldVal:term $[,]?]* }) =>
+    `($mods:declModifiers def $id:declId $sig:optDeclSig where
+        $[$fieldName:ident := $fieldVal:term]*)
 
-open Lean.Parser.Command in
-mathport_rules
-  | `(whereStructField| $id:ident := fun $xs:ident* => $val:term) =>
-    `(whereStructField| $id:ident $xs:ident* := $val:term)
+-- TODO: this seems to break with mathport-generated lambdas
+-- open Lean.Parser.Command in
+-- mathport_rules
+--   | `(whereStructField| $id:ident := fun $xs:ident* => $val:term) =>
+--     `(whereStructField| $id:ident $xs:ident* := $val:term)
--- Mathport/Syntax/Transform/Tactic.lean
+++ Mathport/Syntax/Transform/Tactic.lean
@@ -15,6 +15,14 @@ def transformConsecutiveTactics : Syntax → Syntax → M Syntax
     `(tactic| suffices $ty:term by $seq:tacticSeq)
   | `(tactic| have $[$id:ident]? $[: $ty:term]?), `(tactic|· $seq:tacticSeq) =>
     `(tactic| have $[$id:ident]? $[: $ty:term]? := by $seq:tacticSeq)
+  | `(tactic| have $[$id:ident]? $[: $ty:term]?), `(tactic|exact $t) =>
+    `(tactic| have $[$id:ident]? $[: $ty:term]? := $t)
+  | `(tactic| let $id:ident $[: $ty:term]?), `(tactic|· $seq:tacticSeq) =>
+    `(tactic| let $id:ident $[: $ty:term]? := by $seq:tacticSeq)
+  | `(tactic| suffices : $ty:term), `(tactic|· $seq:tacticSeq) =>
+    `(tactic| suffices $ty:term by $seq:tacticSeq)
+  | `(tactic| obtain $[$pat]? $[: $ty]?), `(tactic|· $seq:tacticSeq) =>
+    `(tactic| obtain $[$pat]? $[: $ty]? := by $seq:tacticSeq)
   | _, _ => throwUnsupported
 
 mathport_rules
@@ -25,3 +33,29 @@ mathport_rules
         let tacs' := tacs[0:i-1] ++ #[tacs[i].setArg 0 tac'] ++ tacs[i+1:tacs.size]
         return Syntax.node info ``Parser.Tactic.tacticSeq1Indented #[Syntax.node info2 `null tacs']
     throwUnsupported
+
+-- common obsolete patterns from haveI
+mathport_rules
+  | `(by have $hd:haveDecl; exact $t) => `(have $hd:haveDecl; $t)
+  | `(by have $hd:haveDecl <;> exact $t) => `(have $hd:haveDecl; $t)
+
+-- used in Lean 3 to postpone elaboration, now happens by default
+mathport_rules | `(by exact $t) => t
+
+mathport_rules | `(by · $seq:tacticSeq) => `(by $seq:tacticSeq)
+
+-- expand `by (skip; skip)` to `by skip; skip`
+mathport_rules
+  | Syntax.node info ``Parser.Tactic.tacticSeq1Indented #[Syntax.node info2 `null tacs] => do
+    let mut tacs' := #[]
+    let mut modified := false
+    for tac in tacs do
+      match tac[0] with
+      | `(tactic| ($seq:tacticSeq1Indented)) =>
+        tacs' := tacs' ++ seq[0].getArgs
+        modified := true
+      | _ => tacs' := tacs'.push tac
+    if modified then
+      Syntax.node info ``Parser.Tactic.tacticSeq1Indented #[Syntax.node info2 `null tacs']
+    else
+      throwUnsupported
