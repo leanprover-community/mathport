68db7f0b1afd559f3de36abd41b21fea632236ff
Mario Carneiro <di.gama@gmail.com>
2021-12-28 02:08:18 -0500

    update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package mathport {
     -- as changes to tactics in mathlib4 may cause breakages here.
     -- Please ensure that `lean-toolchain` points to the same release of Lean 4
     -- as this commit of mathlib4 uses.
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "893a57bed587638f42836cdd67b31090a989a9e8"
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "c11fbda2470fda8c7dbd71d3e9c20b3195cce5b6"
   }],
   binRoot := `MathportApp
   moreLinkArgs := if Platform.isWindows then #[] else #["-rdynamic"]
