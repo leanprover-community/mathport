7cdcace0b975a1e7a9793af3cfc3283774215014
Gabriel Ebner <gebner@gebner.org>
2022-01-19 15:23:40 +0100

    chore: bump to nightly-2022-01-19

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package mathport {
     -- as changes to tactics in mathlib4 may cause breakages here.
     -- Please ensure that `lean-toolchain` points to the same release of Lean 4
     -- as this commit of mathlib4 uses.
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "7046e135615e8b879cfda37104aae7b0ec2c914b"
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "fe835a126539f55ce9dcc5705575d01eb1057625"
   }],
   binRoot := `MathportApp
   moreLinkArgs := if Platform.isWindows then #[] else #["-rdynamic"]
