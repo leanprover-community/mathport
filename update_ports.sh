a4930384c1631f797bb4a73729520e79312d11f2
Gabriel Ebner <gebner@gebner.org>
2022-11-07 14:48:23 -0800

    ci: 3rd try to fix release

--- update_ports.sh
+++ update_ports.sh
@@ -4,8 +4,8 @@ GITHUB_USER=leanprover-community-bot
 
 set -ex
 
-git config user.email "leanprover.community@gmail.com"
-git config user.name "leanprover-community-bot"
+git config --global user.email "leanprover.community@gmail.com"
+git config --global user.name "leanprover-community-bot"
 
 git clone "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/leanprover-community/lean3port"
 pushd lean3port
