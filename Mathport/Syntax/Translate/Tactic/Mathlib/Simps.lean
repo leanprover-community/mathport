cb3d5a72795a7d1049234eb0bd7a2d91bc8b81ed
Gabriel Ebner <gebner@gebner.org>
2023-02-28 15:50:55 -0800

    chore: bump mathlib

--- Mathport/Syntax/Translate/Tactic/Mathlib/Simps.lean
+++ Mathport/Syntax/Translate/Tactic/Mathlib/Simps.lean
@@ -16,17 +16,21 @@ open Parser
   let star := optTk star.isSome
   `(attr| notation_class $[*%$star]? $[$(← liftM do n.mapM mkIdentF)]?)
 
-def trSimpsRule : Sum (Name × Name) Name × Bool → M (TSyntax ``Parser.Command.simpsRule)
+def trSimpsRule : Sum (Name × Name) Name × Bool → M (Array (TSyntax ``Parser.Command.simpsRule))
   | (arg, pfx) => do
-    let pfx := optTk pfx
-    match arg with
-    | .inl (a, b) => `(Parser.Command.simpsRule| $(← mkIdentF a):ident → $(← mkIdentF b) $[as_prefix%$pfx]?)
-    | .inr a => `(Parser.Command.simpsRule| - $(← mkIdentF a):ident $[as_prefix%$pfx]?)
+    let (rhs, rule) ← match arg with
+      | .inl (a, b) =>
+        let rhs ← mkIdentF b
+        Prod.mk rhs <$> `(Parser.Command.simpsRule| $(← mkIdentF a):ident → $rhs)
+      | .inr a =>
+        let rhs ← mkIdentF a
+        Prod.mk rhs <$> `(Parser.Command.simpsRule| - $rhs)
+    if pfx then return #[rule, ← `(Parser.Command.simpsRule| as_prefix $rhs)] else return #[rule]
 
 @[tr_user_cmd «initialize_simps_projections»] def trInitializeSimpsProjections : Parse1 Unit :=
   parse1 (return (← (tk "?")?, ← (return (← ident, ← simpsRules))*)) fun (trc, projs) => do
     for (n, rules) in projs do
-      let rules ← liftM do rules.mapM trSimpsRule
+      let rules ← liftM do rules.concatMapM trSimpsRule
       if trc.isSome then
         pushM `(initialize_simps_projections? $(← mkIdentF n):ident ($[$rules],*))
       else
