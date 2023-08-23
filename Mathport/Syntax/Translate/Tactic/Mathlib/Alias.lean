f982a2b2e8f89cca420200204821f5643aeedabb
Mario Carneiro <di.gama@gmail.com>
2023-08-23 17:16:58 -0400

    chore: update lean

--- Mathport/Syntax/Translate/Tactic/Mathlib/Alias.lean
+++ Mathport/Syntax/Translate/Tactic/Mathlib/Alias.lean
@@ -13,22 +13,29 @@ open AST3 Parser
 
 -- # tactic.alias
 
-@[tr_user_cmd «alias»] def trAlias (doc : Option String) : Parse1 Syntax.Command :=
+@[tr_user_cmd «alias»] def trAlias (doc : Option String) : Parse1 Unit :=
   parse1 (return (← ident <* withInput skipAll, ←
     (tk "<-" *> Sum.inl <$> ident*) <|>
     ((tk "↔" <|> tk "<->") *> Sum.inr <$>
       ((tk ".." *> pure none) <|> return some (← ident_, ← ident_)))))
-  fun (old, args) => do
+  fun (name, args) => do
     let doc := doc.map trDocComment
-    let old ← mkIdentI old
+    let old ← mkIdentI name
     match args with
     | Sum.inl ns =>
-      ns.forM (do _ ← trDeclId · none .regular false)
-      `(command| $[$doc]? alias $old ← $(← liftM $ ns.mapM mkIdentI)*)
-    | Sum.inr none =>
-      warn! "warning: don't know how to generate #align statements for .."
-      `(command| $[$doc]? alias $old ↔ ..)
-    | Sum.inr (some (l, r)) => do
+      for n in ns do
+        _ ← trDeclId n none .regular false
+        push (← `(command| $[$doc]? alias $(← mkIdentI n) := $old))
+    | Sum.inr args =>
+      let (l, r) ← args.getDM do
+        let .str parent base := name
+          | warn! "alias only works for string names" | pure (.«_», .«_»)
+        let components := base.splitOn "_iff_"
+        let forward := String.intercalate "_of_" components.reverse
+        let backward := String.intercalate "_of_" components
+        let forwardName := Name.mkStr parent forward
+        let backwardName := Name.mkStr parent backward
+        pure (.ident forwardName, .ident backwardName)
       if let .ident li := l then _ ← trDeclId li none .regular false
       if let .ident ri := r then _ ← trDeclId ri none .regular false
-      `(command| $[$doc]? alias $old ↔ $(← trBinderIdentI l) $(← trBinderIdentI r))
+      push <|← `(command| $[$doc]? alias ⟨$(← trBinderIdentI l), $(← trBinderIdentI r)⟩ := $old)
