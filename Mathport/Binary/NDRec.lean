16d6ee5dd6fecd307e756051dbd6ec3725eb331c
Mario Carneiro <di.gama@gmail.com>
2023-08-31 03:58:07 -0400

    chore: update lean

--- Mathport/Binary/NDRec.lean
+++ Mathport/Binary/NDRec.lean
@@ -11,21 +11,21 @@ namespace Mathport.Binary
 open Lean Lean.Meta
 
 def mkNDRec (indTy ndRecName : Name) : MetaM (Option Declaration) := do
-  let some (ConstantInfo.inductInfo indI) ← getConst? indTy | throwError (toString indTy)
+  let indI ← getConstInfoInduct indTy
   let indTy' ← inferType (mkConst indI.name (indI.levelParams.map mkLevelParam))
   let useDepElim ← forallTelescopeReducing indTy' $ fun _ indSort => do
     let .sort level := indSort | throwError (toString indSort)
     pure $ level.normalize != levelZero
   if useDepElim then return none
 
-  let some (ConstantInfo.recInfo recI) ← getConst? (indTy ++ `rec) | throwError (toString $ indTy ++ `rec)
-  let crec := mkConst recI.name (recI.levelParams.map mkLevelParam);
-  let recTy ← inferType crec;
+  let recI ← getConstInfoRec (indTy ++ `rec)
+  let crec := mkConst recI.name (recI.levelParams.map mkLevelParam)
+  let recTy ← inferType crec
   forallTelescopeReducing recTy $ fun args _ => do
-    let (params, args) := args.splitAt recI.numParams;
-    let (motive, args) := args.splitAt 1;
-    let motive := motive.get! 0;
-    let motiveTy ← inferType motive;
+    let (params, args) := args.splitAt recI.numParams
+    let (motive, args) := args.splitAt 1
+    let motive := motive.get! 0
+    let motiveTy ← inferType motive
     forallTelescopeReducing motiveTy $ fun _ elimSort => do
       let .sort elimLevel := elimSort | throwError (toString elimSort)
       let (_minorPremises, args) := args.splitAt recI.numMinors
